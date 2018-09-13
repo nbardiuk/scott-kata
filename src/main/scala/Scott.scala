2007aa1a683913bcb4502fc6e9fa8b53d48cf178
Nazarii Bardiuk <>
2018-09-13 05:19:05 +0100

    split setup and solution

--- src/main/scala/Scott.scala
+++ src/main/scala/Scott.scala
@@ -16,109 +16,61 @@ object Scott {
     def apply[B]: (=> B, (A, SList[A]) => B) => B
   }
 
-  def toTuple[A, B](tuple: STuple[A, B]): (A, B) =
-    tuple[(A, B)]((a, b) => (a, b))
+  def toTuple[A, B](tuple: STuple[A, B]): (A, B) = ???
 
-  def fromTuple[A, B](tuple: (A, B)): STuple[A, B] = new STuple[A, B] {
-    def apply[C] = _ (tuple._1, tuple._2)
-  }
+  def fromTuple[A, B](tuple: (A, B)): STuple[A, B] = ???
 
-  def fst[A, B](tuple: STuple[A, B]): A =
-    tuple[A]((a, _) => a)
+  def fst[A, B](tuple: STuple[A, B]): A = ???
 
-  def snd[B](tuple: STuple[_, B]): B =
-    tuple[B]((_, b) => b)
+  def snd[B](tuple: STuple[_, B]): B = ???
 
-  def swap[A, B](tuple: STuple[A, B]): STuple[B, A] =
-    tuple[STuple[B, A]]((a, b) => new STuple[B, A] {
-      def apply[C] = _ (b, a)
-    })
+  def swap[A, B](tuple: STuple[A, B]): STuple[B, A] = ???
 
-  def curry[A, B, C](f: STuple[A, B] => C): A => B => C =
-    a => b => f(new STuple[A, B] {
-      def apply[C] = _ (a, b)
-    })
+  def curry[A, B, C](f: STuple[A, B] => C): A => B => C = ???
 
-  def uncurry[A, B, C](f: A => B => C): STuple[A, B] => C =
-    tuple => tuple[C](Function.uncurried(f))
+  def uncurry[A, B, C](f: A => B => C): STuple[A, B] => C = ???
 
-  def toOption[A](option: SOption[A]): Option[A] =
-    option[Option[A]](Option.empty[A], Some(_))
+  def toOption[A](option: SOption[A]): Option[A] = ???
 
-  def fromOption[A](option: Option[A]): SOption[A] = new SOption[A] {
-    def apply[B] = (ifEmpty, f) => option.fold(ifEmpty)(f)
-  }
+  def fromOption[A](option: Option[A]): SOption[A] = ???
 
-  def isSome(option: SOption[_]): Boolean =
-    option[Boolean](false, _ => true)
+  def isSome(option: SOption[_]): Boolean = ???
 
-  def isNone(option: SOption[_]): Boolean =
-    option[Boolean](true, _ => false)
+  def isNone(option: SOption[_]): Boolean = ???
 
-  def catOptions[A](list: SList[SOption[A]]): SList[A] =
-    list[SList[A]](nil[A], (head, tail) =>
-      head[SList[A]](catOptions(tail), a => cons(a, catOptions(tail))))
+  def catOptions[A](list: SList[SOption[A]]): SList[A] = ???
 
-  def toEither[A, B](either: SEither[A, B]): Either[A, B] =
-    either[Either[A, B]](Left[A, B], Right[A, B])
+  def toEither[A, B](either: SEither[A, B]): Either[A, B] = ???
 
-  def fromEither[A, B](either: Either[A, B]): SEither[A, B] = new SEither[A, B] {
-    def apply[C] = (left, right) => either.fold(left, right)
-  }
+  def fromEither[A, B](either: Either[A, B]): SEither[A, B] = ???
 
-  def isLeft[A](either: SEither[A, _]): Boolean =
-    either[Boolean](_ => true, _ => false)
+  def isLeft[A](either: SEither[A, _]): Boolean = ???
 
-  def isRight[A](either: SEither[A, _]): Boolean =
-    either[Boolean](_ => false, _ => true)
+  def isRight[A](either: SEither[A, _]): Boolean = ???
 
-  def nil[A]: SList[A] = new SList[A] {
-    def apply[B] = (b, _) => b
-  }
+  def nil[A]: SList[A] = ???
 
-  def toList[A](list: SList[A]): List[A] =
-    list[List[A]](List[A](), (head, tail) => head :: toList(tail))
+  def toList[A](list: SList[A]): List[A] = ???
 
-  def fromList[A](list: List[A]): SList[A] =
-    list.foldRight(nil[A])(cons)
+  def fromList[A](list: List[A]): SList[A] = ???
 
-  def cons[A](head: A, list: SList[A]): SList[A] = new SList[A] {
-    def apply[B] = (_, f) => f(head, list)
-  }
+  def cons[A](head: A, list: SList[A]): SList[A] = ???
 
-  def concat[A](left: SList[A], right: SList[A]): SList[A] =
-    left[SList[A]](right, (leftHead, rightHead) => new SList[A] {
-      def apply[B] = (_, f) => f(leftHead, concat(rightHead, right))
-    })
+  def concat[A](left: SList[A], right: SList[A]): SList[A] = ???
 
-  def empty(list: SList[_]): Boolean =
-    list[Boolean](true, (_, _) => false)
+  def empty(list: SList[_]): Boolean = ???
 
-  def length(list: SList[_]): Int =
-    list[Int](0, (_, tail) => 1 + length(tail))
+  def length(list: SList[_]): Int = ???
 
-  def map[A, B](f: (A => B), list: SList[A]): SList[B] =
-    list[SList[B]](nil[B], (head, tail) => cons(f(head), map(f, tail)))
+  def map[A, B](f: (A => B), list: SList[A]): SList[B] = ???
 
-  def zip[A, B](listA: SList[A], listB: SList[B]): SList[STuple[A, B]] =
-    listA[SList[STuple[A, B]]](nil[STuple[A, B]],
-      (headA, tailA) => listB[SList[STuple[A, B]]](nil[STuple[A, B]],
-        (headB, tailB) => cons(fromTuple(headA, headB), zip(tailA, tailB))))
+  def zip[A, B](listA: SList[A], listB: SList[B]): SList[STuple[A, B]] = ???
 
-  def foldLeft[A, B](f: ((B, A) => B), z: B, list: SList[A]): B =
-    list[B](z, (head, tail) => foldLeft(f, f(z, head), tail))
+  def foldLeft[A, B](f: ((B, A) => B), z: B, list: SList[A]): B = ???
 
-  def foldRight[A, B](f: ((A, B) => B), z: B, list: SList[A]): B =
-    list[B](z, (head, tail) => f(head, foldRight(f, z, tail)))
+  def foldRight[A, B](f: ((A, B) => B), z: B, list: SList[A]): B = ???
 
-  def take[A](n: Int, list: SList[A]): SList[A] =
-    if (n == 0) nil[A]
-    else list[SList[A]](nil[A], (head, tail) => cons(head, take(n - 1, tail)))
+  def take[A](n: Int, list: SList[A]): SList[A] = ???
 
-  def partition[A, B](list: SList[SEither[A, B]]): STuple[SList[A], SList[B]] =
-    list[STuple[SList[A], SList[B]]](fromTuple(nil[A], nil[B]),
-      (head, tail) => partition(tail)[STuple[SList[A], SList[B]]](
-        (leftList, rightList) => head[STuple[SList[A], SList[B]]](
-          leftHead => fromTuple(cons(leftHead, leftList), rightList),
-          rightHead => fromTuple(leftList, cons(rightHead, rightList)))))
+  def partition[A, B](list: SList[SEither[A, B]]): STuple[SList[A], SList[B]] = ???
 }
